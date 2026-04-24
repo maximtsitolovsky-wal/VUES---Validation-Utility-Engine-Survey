df2e5fc127c20d50d7c8817a87bc552bb6a3e83b
Maxim Tsitolovsky - Vendor <vn59j7j@homeoffice.wal-mart.com>
2026-04-24 09:18:23 -0500

    auto: 2026-04-24 09:18:23 | rc/siteowlqa/python_grader.py

--- src/siteowlqa/python_grader.py
+++ src/siteowlqa/python_grader.py
@@ -27,8 +27,9 @@ from siteowlqa.config import (
     SURVEY_TYPE_CCTV,
     SURVEY_TYPE_FA_INTRUSION,
     SURVEY_TYPE_BOTH,
-    FA_INTRUSION_NAME_CONDITION_COLUMN,
     get_grade_columns_for_survey_type,
+    get_name_condition_column,
+    get_base_grade_columns,
 )
 from siteowlqa.models import ProcessingStatus, SubmissionResult
 from siteowlqa.reference_data import fetch_reference_rows

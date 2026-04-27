3dd959eadd14a153315e1ffc0d58a7a786aa8213
Maxim Tsitolovsky - Vendor <vn59j7j@homeoffice.wal-mart.com>
2026-04-27 12:43:59 -0500

    chore: clean up temp debug scripts

--- scripts/_check_config.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, 'src')
-from siteowlqa.config import AppConfig
-cfg = AppConfig()
-print('scout_airtable_view_id:', repr(cfg.scout_airtable_view_id))
-print('scout_airtable_base_id:', repr(cfg.scout_airtable_base_id))
-print('scout_airtable_table_name:', repr(cfg.scout_airtable_table_name))
